be88b8dcb7a23bc31c04568d57c60c2b0e4f2cbb
Inna Soboleva <nysik62@gmail.com>
2023-05-20 23:21:33 -0700

    Added comments for  melon_info.py.

--- accounting-scripts/melon_info.py
+++ accounting-scripts/melon_info.py
@@ -5,11 +5,14 @@ from melons import melons
 
 
 def print_melon_info(dict):
-     """Print each melon with corresponding attribute information."""
+     """Print each melon with corresponding attribute information.
+     
+        Function takes dictionary as an argument with all melon's data.
+     """
 
      for melon in dict:
-        print(melon.upper())
-        for key in dict[melon].keys():
+        print(melon.upper())    # getting melon's name
+        for key in dict[melon].keys():      # getting all data (keys) for each melon
              print(f"{key}: {dict[melon][key]}")
         print()
 
