d7729256a6cc1385ee7be3fe17d5578ab7c01aa7
Inna Soboleva <nysik62@gmail.com>
2023-05-20 23:18:41 -0700

    Changed melon_info.py for printing correct output.

--- accounting-scripts/melon_info.py
+++ accounting-scripts/melon_info.py
@@ -8,9 +8,9 @@ def print_melon_info(dict):
      """Print each melon with corresponding attribute information."""
 
      for melon in dict:
-         print(melon)
-         for key, value in enumerate(melon):
-             print(f"{key}: {value}")
-
+        print(melon.upper())
+        for key in dict[melon].keys():
+             print(f"{key}: {dict[melon][key]}")
+        print()
 
 print_melon_info(melons)
